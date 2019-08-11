2b2a72120f506ee7d6b5ac306396be63bef5f592
Alif Momin <alifmomin1234567890@gmail.com>
2019-08-11 19:38:52 +0000

    quiz2

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,7 @@
 class IdeasController < ApplicationController
 
   def index
+    @lyrics = Lyric.all
   end
 
 end
---
+++ db/migrate/20190811184517_create_lyrics.rb
@@ -0,0 +1,11 @@
+class CreateLyrics < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lyrics do |t|
+      t.string :name
+      t.text :description
+      t.timestamps
+    end
+  end
+end
+
+
